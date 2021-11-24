ea1e36abfb0d5f296ed7271a57238906cf23fa90
fico45 <novosel.filip45@gmail.com>
2021-11-24 13:20:09 +0100

    String adjustment
    I merely adjusted a placeholder string. Nothing to see here. :D

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -11,7 +11,7 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Comics'),
+      child: Text('Favorites'),
     );
   }
 }
